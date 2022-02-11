2e1357b9d864f0c2542e2bef29013c81643956e0
qodhrkawk <gabriel1120@naver.com>
2022-02-11 21:34:06 +0900

    Project Initialize

---
+++ PairChecker/PairChecker/NameChecker.swift
@@ -0,0 +1,21 @@
+//
+//  NameChecker.swift
+//  PairChecker
+//
+//  Created by Yunjae Kim on 2022/02/11.
+//
+
+import Foundation
+
+class NameChecker {
+    
+    func sliceHangeul(hangeul: Character) -> [Character] {
+        guard hangeul > "가" && hangeul < "힣" else { return []}
+        
+        let cho = 
+        
+        return []
+    }
+    
+    
+}
